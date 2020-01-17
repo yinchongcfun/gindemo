ed23aa6c0a42f71edeeda27f0361c51c275648aa
cfun <1606548133@qq.com>
2020-01-17 18:01:43 +0800

    加入日志框架

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"gindemo/config"
 	"gindemo/database"
+	hook "gindemo/log"
 	"gindemo/public"
 	"gindemo/router"
-	"github.com/sirupsen/logrus"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
 )
 
-var log = logrus.New()
+func initLog() {
+	uuids := uuid.NewV1()
+	log.AddHook(hook.NewTraceIdHook(uuids.String() +" "))
+	timeStr:=time.Now().Format("2006-01-02")
+	file, _ := os.OpenFile("log/"+timeStr+".log", os.O_CREATE|os.O_WRONLY, 0666)
+	log.SetOutput(file)
+	//设置最低loglevel
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
+	initLog()
 	config.InitConfig()
 	database.InitMysql()
 	database.InitRedis()
