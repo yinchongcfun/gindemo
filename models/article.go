c258780739687f4bf2fcc1d8798c196fe34d605b
cfun <1606548133@qq.com>
2020-09-12 10:52:58 +0800

    第一次删除
    第二次删除
    
    第三次删除

--- models/article.go
+++ models/article.go
@@ -28,16 +28,16 @@ func (a *Article) ListArticle1(title string) (articles []Article, err error) {
 	return
 }
 
-func ListArticle2(title string) (articles []Article, err error) {
-	query := database.GormPool
-	err = query.Model(articles).
-		Where("title like ?", "%"+title+"%").
-		Preload("Category").Find(&articles).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return
-	}
-	return
-}
+//func ListArticle2(title string) (articles []Article, err error) {
+//	query := database.GormPool
+//	err = query.Model(articles).
+//		Where("title like ?", "%"+title+"%").
+//		Preload("Category").Find(&articles).Error
+//	if err != nil && err != gorm.ErrRecordNotFound {
+//		return
+//	}
+//	return
+//}
 
 func (a *Article) ListArticle(title string) (Article, error) {
 	query := database.GormPool
@@ -50,31 +50,31 @@ func (a *Article) ListArticle(title string) (Article, error) {
 	return article, err
 }
 
-func (a *Article) ListArticle3(title string) (Article, error) {
-	query := database.GormPool
-	var article Article
-	query.Where("title like ?", "%"+title+"%").First(&article)
-	err := query.Model(&article).Association("Category").Find(&article.Category).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return article, nil
-	}
-	return article, err
-}
+//func (a *Article) ListArticle3(title string) (Article, error) {
+//	query := database.GormPool
+//	var article Article
+//	query.Where("title like ?", "%"+title+"%").First(&article)
+//	err := query.Model(&article).Association("Category").Find(&article.Category).Error
+//	if err != nil && err != gorm.ErrRecordNotFound {
+//		return article, nil
+//	}
+//	return article, err
+//}
 
-//远程一对多
-func (a *Article) ListArticle4(title string) (Article, error) {
-	query := database.GormPool
-	var article Article
-	query.Where("title like ?", "%"+title+"%").First(&article)
-	err := query.Model(&article).
-		Related(&article.Category).
-		Related(&article.Tag, "tag").
-		Find(&article).Error
-	if err != nil && err != gorm.ErrRecordNotFound {
-		return article, nil
-	}
-	return article, err
-}
+////远程一对多
+//func (a *Article) ListArticle4(title string) (Article, error) {
+//	query := database.GormPool
+//	var article Article
+//	query.Where("title like ?", "%"+title+"%").First(&article)
+//	err := query.Model(&article).
+//		Related(&article.Category).
+//		Related(&article.Tag, "tag").
+//		Find(&article).Error
+//	if err != nil && err != gorm.ErrRecordNotFound {
+//		return article, nil
+//	}
+//	return article, err
+//}
 
 //多对多
 //func (a *Article) ListArticle(title string) (articles []Article, err error) {
